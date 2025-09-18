c593e4450965e1eaff8405f7b2cc807aedc14d18
Felle26 <sebastianfelsberg@web.de>
2025-09-18 22:16:03 +0200

    rename file input to COMMIT_EDITMSG

--- src/Content/body/Body_comp.jsx
+++ src/Content/body/Body_comp.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import Home_comp from './Pages/Home_comp.jsx'
 import Aboutme_comp from './Pages/about/aboutme_comp.jsx'
 import Projects_comp from './Pages/projects/Projects_comp.jsx'
-import Contact_comp from './Pages/contact/Contact_comp.jsx'
+import Contact_comp from './Pages/Contact/Contact_comp.jsx'
 import Impressum from './Pages/impressum.jsx'
 
 const Body_comp = ({ currentSection, setCurrentSection }) => {
