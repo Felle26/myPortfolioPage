9e8d42a5835c236300f4b8974bdae56a459da1b5
Felle26 <sebastianfelsberg@web.de>
2025-09-18 22:18:37 +0200

    rename 2

--- src/Content/body/Body_comp.jsx
+++ src/Content/body/Body_comp.jsx
@@ -4,7 +4,7 @@ import Home_comp from './Pages/Home_comp.jsx'
 import Aboutme_comp from './Pages/about/aboutme_comp.jsx'
 import Projects_comp from './Pages/projects/Projects_comp.jsx'
 import Contact_comp from './Pages/Contact/Contact_comp.jsx'
-import Impressum from './Pages/impressum.jsx'
+import Impressum from './Pages/Impressum.jsx'
 
 const Body_comp = ({ currentSection, setCurrentSection }) => {
   return (
